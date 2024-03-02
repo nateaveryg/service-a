ceb3bc76576d9f40e154fd5d2b34a80d941b17c9
nateaveryg <103460947+nateaveryg@users.noreply.github.com>
2024-03-01 21:20:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,5 +11,4 @@ def index():
     return jsonify({'message': 'Hello from service-a'})
 
 if __name__ == "__main__":
-    app.run()
-#    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
